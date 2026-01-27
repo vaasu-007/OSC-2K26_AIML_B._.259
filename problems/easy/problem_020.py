e10920605e49f9f76baa9d2905fdd2f30b16f72f
vaasu-007 <vasanth.k_25am@kgkite.ac.in>
2026-01-27 08:40:17 +0530

    problem_020_solved

--- problems/easy/problem_020.py
+++ problems/easy/problem_020.py
@@ -13,5 +13,5 @@ Difficulty: Easy
 
 price = 100
 discount_percent = 10
-discounted_price = price * (1 - discount_percent) # Should be discount_percent / 100
+discounted_price = price * (1 - discount_percent / 100) # Should be discount_percent / 100
 print(discounted_price)
